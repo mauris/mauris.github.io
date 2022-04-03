ca6721864637f292e4a78f6dd7ec661ed1edd496
Sam <samyonggit@gmail.com>
2022-04-03 18:03:51 +0800

    add favicon and meta

--- apps/main/pages/404.tsx
+++ apps/main/pages/404.tsx
@@ -5,7 +5,7 @@ export default function Custom404() {
   return (
     <SiteLayout title="404 Not Found">
       <div className="py-24">
-        <h1 className="text-2xl md:text-3xl font-normal">The page you&apos;re looking for is not found.</h1>
+        <h1 className="text-2xl md:text-3xl font-normal">The page you&apos;re looking for was not found.</h1>
         <p className="text-neutral-500">
           Try browsing from <Link href="/">Home</Link>.
         </p>
