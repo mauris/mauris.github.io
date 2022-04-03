5d478cb92f42487829402d1626d0e2ebd96752a5
Sam <samyonggit@gmail.com>
2022-04-03 11:49:10 +0800

    add 404 page

---
+++ apps/main/pages/404.tsx
@@ -0,0 +1,15 @@
+import SiteLayout from '@self/components/SiteLayout';
+import Link from 'next/link';
+
+export default function Custom404() {
+  return (
+    <SiteLayout title="404 Not Found">
+      <div className="py-24">
+        <h1 className="text-2xl md:text-3xl font-normal">The page you're looking for is not found.</h1>
+        <p className="text-neutral-500">
+          Try browsing from <Link href="/">Home</Link>.
+        </p>
+      </div>
+    </SiteLayout>
+  );
+}
