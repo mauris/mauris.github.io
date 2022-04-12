70db4309fa6b9fb1b7420339dc73bb1ee1a2f3c1
Sam Yong <samyonggit@gmail.com>
2022-04-12 22:28:58 +0800

    fix codeblock

--- apps/main/components/CodeBlock.tsx
+++ apps/main/components/CodeBlock.tsx
@@ -1,13 +1,14 @@
 import { PropsWithChildren } from 'react';
-import { CodeProps } from 'react-markdown/lib/ast-to-react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import styles from './CodeBlock.module.scss';
 
-export default function CodeBlock({ node, inline, className, children, ...props }: PropsWithChildren<CodeProps>) {
+export default function CodeBlock(originalProps: PropsWithChildren<JSX.IntrinsicElements['code']>) {
+  console.log(originalProps);
+  const { className, children, ...props } = originalProps;
   const match = /language-(\w+)/.exec(className || '');
-  return !inline && match ? (
+  return match ? (
     <div className={styles.codeBlock}>
       <SyntaxHighlighter style={vscDarkPlus} language={match[1]} PreTag="div" {...props}>
         {String(children).replace(/\n$/, '')}
